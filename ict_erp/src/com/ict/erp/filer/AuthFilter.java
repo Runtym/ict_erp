151be25c211ee4e71b0cf28c52413d1ccb97d883
koitt03-A <koitt03-A@koitt03-A-PC>
2018-09-06 17:25:56 +0900

    reflection test

--- ict_erp/src/com/ict/erp/filer/AuthFilter.java
+++ ict_erp/src/com/ict/erp/filer/AuthFilter.java
@@ -28,18 +28,9 @@ public class AuthFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest hsr = (HttpServletRequest)request;
 		HttpSession hs = hsr.getSession();
-		//1. 트레이스
-		log.trace(hsr.getRequestURI());
-		//2. 디버그
-		log.debug(hsr.getRequestURI());
+		
 		//3. 인포
-		log.info(hsr.getRequestURI());
-		//4. 워닝
-		log.warn(hsr.getRequestURI());
-		//5. 에러
-		log.error(hsr.getRequestURI());
-		//6. 겁내 심각한 에러
-		log.fatal(hsr.getRequestURI()); 
+		log.info(hsr.getMethod() + ":" + hsr.getRequestURI());
 		if(hs.getAttribute("user")==null) {
 			// TODO 권한 처리 필요.(로그인 페이지 이동 로직 추가 필요)
 			System.out.println("너 권한 없음");
